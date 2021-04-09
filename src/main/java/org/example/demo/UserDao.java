1a0d796a6c81e43673896bc5de24cafae4dea654
Mushegh <asddsajpg@gmail.com>
2021-04-09 22:15:46 +0400

    first commit

---
+++ src/main/java/org/example/demo/UserDao.java
@@ -0,0 +1,16 @@
+package org.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public UserDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+}
